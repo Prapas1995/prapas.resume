02fbde7f05191862dbd8e37382e0e4744b1f5545
Prapas1995 <Prapasjongkaenboon@gmail.com>
2025-07-14 17:23:54 +0700

    merge

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/prapas.resume/'
+  publicPath: '/prapas.resume/',
+  lintOnSave: false,
 })
