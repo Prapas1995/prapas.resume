92b04391fa587d3da8b767cde246ca1cf1ac214a
Prapas1995 <Prapasjongkaenboon@gmail.com>
2025-07-14 18:07:20 +0700

    merge

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="homepage">
+      <HelloWorld/>
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  export default {
+    components: {
+        HelloWorld: () => import('@/components/HelloWorld.vue')
+    }
+  }
+  </script>
+  
