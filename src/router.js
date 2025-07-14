5fd80138d237b59a6dc8d3bc39ad7271ba6c1697
Prapas1995 <Prapasjongkaenboon@gmail.com>
2025-07-14 17:33:57 +0700

    merge

---
+++ src/router.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Meta from 'vue-meta'
+
+Vue.use(VueRouter)
+Vue.use(Meta)
+
+const Router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+//   scrollBehavior (to, from, savedPosition) {
+//     return { x: 0, y: 0 }
+//   },
+  routes: [
+    {
+      path: '/',
+      component: () => import('./pages/index.vue'),
+      children: [
+        {
+          name: 'resume',
+          path: '/',
+          component: () => import('./pages/index.vue'),
+          meta: {
+            title: 'Resume',
+          }
+        }
+      ]
+    }
+  ]
+})
+
+Router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
+})
+
+export default Router
